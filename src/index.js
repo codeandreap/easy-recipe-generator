3ea7efe31debca10f6d59dc27a6b59b1facd0fcd
codeandreap <anpash72@gmail.com>
2025-08-19 16:20:19 +0100

    added js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = "1 onion chopped";
+  new Typewriter("#recipe", {
+  strings: "1 onion chopped",
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+}
+
+let recipeFormElement = document.querySelector("#ingredient-entry-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
