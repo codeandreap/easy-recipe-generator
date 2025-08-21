089dd7085b5b9d94c5b2bea473c0a6f001cdf8ef
codeandreap <anpash72@gmail.com>
2025-08-21 14:59:57 +0100

    improved UX

--- src/index.js
+++ src/index.js
@@ -13,14 +13,15 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "tbo7620f2867660145cf2bae403e471b";
   let context =
-    "You are a top chef. You provide easy-to-make recipes based on a single ingredient. These recipes should have no more than 10 ingredients. These ingredients should be a staple in anyone's kitchen. These recipes should take no longer than 30-45 minutes to make. Provide this recipe in a standard recipe format using basic HTML. Make sure to follow user instructions.";
+    "You are a top chef. You provide easy-to-make recipes based on a single ingredient. These recipes should have no more than 10 ingredients. These ingredients should be a staple in anyone's kitchen. These recipes should take no longer than 30-45 minutes to make. Provide this recipe in a standard recipe format using basic HTML. Make sure to follow user instructions. Make sure there is no added space at the top of the input box.";
   let prompt = `User instructions are: Generate a recipe that incorporates ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(showRecipe);
 
-  //let recipeElement = document.querySelector("#recipe");
-  //recipeElement.innerHTML = "1 onion chopped";
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `Generating a recipe using ${instructionsInput.value}<span class="blink">...</span>`;
 }
 
 let recipeFormElement = document.querySelector("#ingredient-entry-form");
